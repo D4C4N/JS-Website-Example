a61c82a7fb8caf29528401a0b9fe6abd222e1e93
D4C4N <dacan1508@gmail.com>
2023-10-20 15:01:43 +0200

    Added modals for the sub-menus in the hamburger menu.

--- js/general.js
+++ js/general.js
@@ -1,3 +1,4 @@
+/* Image Animation */
 const track = document.getElementById("image-track");
 
 const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
@@ -30,6 +31,7 @@ const handleOnMove = e => {
   }
 }
 
+/* Hamburger menu */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -44,8 +46,45 @@ document.querySelectorAll(".nav-link").forEach(n => n.
     navMenu.classList.remove("active");
   }))
 
-/* -- Had to add extra lines for touch events -- */
+/* Modal */
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+/* Various */
 window.onmousedown = e => handleOnDown(e);
 
 window.ontouchstart = e => handleOnDown(e.touches[0]);
