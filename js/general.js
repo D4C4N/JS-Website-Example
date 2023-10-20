1e3d243ea516f6fa42703148e44c75fc9fcdbbcd
D4C4N <dacan1508@gmail.com>
2023-10-20 13:47:59 +0200

    Added a navigation bar and animations.

--- js/general.js
+++ js/general.js
@@ -30,6 +30,20 @@ const handleOnMove = e => {
   }
 }
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+  addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
+
 /* -- Had to add extra lines for touch events -- */
 
 window.onmousedown = e => handleOnDown(e);
